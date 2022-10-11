1a5c8a3bb2a586dc1175c4cdba30cfa8a8ff3eb5
Cristina L <cristinal@MacBook-Pro-de-Cristina.local>
2022-10-11 10:10:33 -0300

    added swtichToNewWindow() method

--- src/test/java/libs/BrowserUtils.java
+++ src/test/java/libs/BrowserUtils.java
@@ -1,5 +1,12 @@
 package libs;
 
 public class BrowserUtils {
-    
+ public void selectByText() {
+     //implementation
+ }
+
+ public void switchToNewWindow(){
+     //implementation done
+
+ }
 }
