5398b7b413faec1c97384d28526e5ad6ac8a62f6
Cristina L <cristinal@MacBook-Pro-de-Cristina.local>
2022-10-11 10:02:29 -0300

    US34 completed

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,6 +1,22 @@
+import jdk.jfr.StackTrace;
+import org.testng.annotations.Test;
+
 public class HomeTest {
     String name = "Cristina";
     String role = "Student";
     int id = 99;
     String email = "cristina@test.com";
+
+    @Test(testName = "Testing title of the page")
+    public void test01(){
+        //implementation
+    }
+
+    @Test
+    public void test02(){
+        //implementation part1
+        //use switchToNewWindow method
+        //completed test case
+    }
+
 }
